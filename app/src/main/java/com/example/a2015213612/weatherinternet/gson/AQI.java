8cf3d7fc52a10ff236c421ae552f845de6746699
zfl1996 <zfl18845893756@163.com>
2018-06-16 07:37:43 +0800

    First commit

---
+++ app/src/main/java/com/example/a2015213612/weatherinternet/gson/AQI.java
@@ -0,0 +1,15 @@
+package com.example.a2015213612.weatherinternet.gson;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
---
+++ app/src/main/java/com/example/a2015213612/weatherinternet/gson/Now.java
@@ -0,0 +1,20 @@
+package com.example.a2015213612.weatherinternet.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Now {
+
+    @SerializedName("tmp")
+    public String temperature;
+
+    @SerializedName("cond")
+    public More more;
+
+    public class More {
+
+        @SerializedName("txt")
+        public String info;
+
+    }
+
+}
